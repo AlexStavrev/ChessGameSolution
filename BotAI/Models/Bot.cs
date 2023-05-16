0f4360da6861de86c3771e06f12b988b942bd48e
AlexStavrev <40688355+AlexStavrev@users.noreply.github.com>
2023-05-16 13:26:28 +0200

    Added strategies to bot ai

--- BotAI/Models/Bot.cs
+++ BotAI/Models/Bot.cs
@@ -10,21 +10,30 @@ public class Bot
     public Guid Id { get; private set; }
     public IGame GameBoard { get; set; }
     public Guid? BoardId { get; set; }
+    public IBotStrategy Strategy { get; set; }
 
+    //private readonly IMessagePublisher _messagePublisher;
 
     public Bot()
     {
         Id = new Guid();
         GameBoard = GameFactory.Create();
         BoardId = null;
+        Strategy = new FirstInMindStrategy();
     }
 
     public Bot(Guid id, IGame gameBoard, BoardSide side, IBotStrategy strategy/*, IMessagePublisher messagePublisher*/)
     {
         Id = id;
         GameBoard = gameBoard;
+        Strategy = strategy;
+        //_messagePublisher = messagePublisher;
     }
 
     }
 
+    public Move GetNextMove()
+    {
+        return Strategy.GetNextMove(GameBoard);
+    }
 }
