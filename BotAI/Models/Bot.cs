45c8aa73a9908b41a92d1f6af83a81af9fe35db1
AlexStavrev <40688355+AlexStavrev@users.noreply.github.com>
2023-05-16 13:19:39 +0200

    Create Bot.cs
    Initial Bot Class

---
+++ BotAI/Models/Bot.cs
@@ -0,0 +1,30 @@
+﻿using BotAI.Strategies;
+using Rudzoft.ChessLib;
+using Rudzoft.ChessLib.Factories;
+using Rudzoft.ChessLib.Types;
+
+namespace BotAI.Models;
+
+public class Bot
+{
+    public Guid Id { get; private set; }
+    public IGame GameBoard { get; set; }
+    public Guid? BoardId { get; set; }
+
+
+    public Bot()
+    {
+        Id = new Guid();
+        GameBoard = GameFactory.Create();
+        BoardId = null;
+    }
+
+    public Bot(Guid id, IGame gameBoard, BoardSide side, IBotStrategy strategy/*, IMessagePublisher messagePublisher*/)
+    {
+        Id = id;
+        GameBoard = gameBoard;
+    }
+
+    }
+
+}
