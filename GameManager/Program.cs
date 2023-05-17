201386f56e1b6dcb478bccfaed0febb42b350914
AlexStavrev <40688355+AlexStavrev@users.noreply.github.com>
2023-05-17 13:41:31 +0200

    Started the listener from Program.cs

--- GameManager/Program.cs
+++ GameManager/Program.cs
@@ -1,2 +1,8 @@
-﻿
+﻿using GameManager.Messaging;
+using System.Text;
 
+string cloudAMQPConnectionString = File.ReadAllText(@"..\..\cloudAMQPConnectionString.txt", Encoding.UTF8);
+
+Task.Factory.StartNew(() =>
+    new MessageSubscriber(cloudAMQPConnectionString).Start()
+);
