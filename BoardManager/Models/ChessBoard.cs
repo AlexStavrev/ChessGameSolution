92821e62288641c10515ba2d772edc46eb4010c3
AdamGalla <ado.galla@gmail.com>
2023-05-17 12:04:50 +0200

    ChessBoard class and register board logic implemented

---
+++ BoardManager/Models/ChessBoard.cs
@@ -0,0 +1,36 @@
+﻿using BoardManager.Messaging;
+using Rudzoft.ChessLib;
+using Rudzoft.ChessLib.Factories;
+using SharedDTOs.DTOs;
+
+namespace BoardManager.Models;
+
+public class ChessBoard
+{
+    public Guid Id { get; set; }
+    public IGame GameBoard { get; set; }
+    public ICollection<BotDTO> Bots { get; set; }
+
+    private readonly IMessagePublisher _messagePublisher;
+
+    public ChessBoard(IMessagePublisher messagePublisher) : this(
+        new Guid(),
+        GameFactory.Create(),
+        new List<BotDTO>(),
+        messagePublisher
+    )
+    { }
+
+    public ChessBoard(Guid id, IGame gameBoard, ICollection<BotDTO> bots, IMessagePublisher messagePublisher) 
+    { 
+        Id = id; 
+        GameBoard = gameBoard; 
+        Bots = bots; 
+        _messagePublisher = messagePublisher;
+    }
+
+    public void RegisterBoard()
+    {
+        _messagePublisher.PublishRegisterBoard(Id);
+    }
+}
